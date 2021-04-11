fdcd79c83711d63c9dfeb7637fb3252fb7a61d36
pouyehbnj <pouyehbnj@gmail.com>
2021-04-11 12:57:43 -0700

    creating weighted graph added

--- graphs.py
+++ graphs.py
@@ -1,3 +1,13 @@
 import pandas as pd
-df = pd.read_csv('./soc-sign-bitcoinalpha.csv',usecols=[0,1,2])
+import numpy as np
+df = pd.read_csv('./soc-sign-bitcoinalpha.csv' ,  names=['source','destination','weight'] ,usecols=[0,1,2])
 print(df)
+name_to_node = {name: i for i, name in enumerate(np.unique(df[["source", "destination"]].values))}
+n_nodes = len(name_to_node)
+A = np.zeros((n_nodes, n_nodes))
+for row in df.itertuples():
+    n1 = name_to_node[row.source]
+    n2 = name_to_node[row.destination]
+    A[n1, n2] += row.weight
+    A[n2, n1] += row.weight
+print(A)
