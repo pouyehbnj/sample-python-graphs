a2024c70ef831384af4f8a0dbd9113395b0d6912
zahra cloudtek <cloudtek.zahra@gmail.com>
2021-04-11 12:25:38 +0430

    dummy test

--- graphs.py
+++ graphs.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import networkx as nx
 df = pd.read_csv('./soc-sign-bitcoinalpha.csv')
-#print(df)
-G = nx.from_pandas_adjacency(df)
+print(df)
+#G = nx.from_pandas_adjacency(df)
+#print(G)
+
+# Data = open('./soc-sign-bitcoinalpha.csv', "r")
+# next(Data, None)  # skip the first line in the input file
+# Graphtype = nx.Graph()
+
+# G = nx.parse_edgelist(Data, delimiter=',', create_using=Graphtype,
+#                       nodetype=int, data=(('weight', float),))
+G = nx.DiGraph()
+
+for d in pd.read_csv('./soc-sign-bitcoinalpha.csv',sep=',', header=None, names=['source', 'destination' , 'weight']):
+    G.add_edges_from([tuple(x) for x in d.values])
+
 print(G)
