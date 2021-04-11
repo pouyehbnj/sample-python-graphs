aaeccaeaf285feadd6b16a020dc6d6358ff79079
zahra cloudtek <cloudtek.zahra@gmail.com>
2021-04-11 14:01:55 +0430

    add avg neighbors

--- graphs.py
+++ graphs.py
@@ -25,38 +25,17 @@ print(Graph)
 print(nx.info(Graph))
 for s in Graph.degree():
     print(s)
-# mat = nx.to_pandas_adjacency(Graph ,weight='weight')
-# print(mat)
-# G=nx.from_pandas_dataframe(df, 'source', 'destination', 'weight')
-# print(G)
-
-print('hi')
 
 
-# print(A)
-# print(df)
-#G = nx.from_pandas_adjacency(df)
-#print(G)
+for node in list(Graph.nodes):
+    numberNeighbors=0 
+    sumDegree=0
+    for neighbor in list(Graph.neighbors(node)):
+        numberNeighbors= numberNeighbors + 1 
+        sumDegree =  Graph.degree(neighbor) + sumDegree
+    avgDegNeighbor = sumDegree/numberNeighbors
+    print("node = " + str(node) + " avg of neighbors is : " + str(avgDegNeighbor))
 
-# Data = open('./soc-sign-bitcoinalpha.csv', "r")
-# next(Data, None)  # skip the first line in the input file
-# Graphtype = nx.Graph()
+        
 
-# G = nx.parse_edgelist(Data, delimiter=',', create_using=Graphtype,
-#                       nodetype=int, data=(('weight', float),))
-# G = nx.DiGraph()
 
-# for d in pd.read_csv('./soc-sign-bitcoinalpha.csv',sep=',', header=None, names=['source', 'destination' , 'weight']):
-#     G.add_edges_from([tuple(x) for x in d.values])
-
-# print(G)
-#df.columns = ['node_1','node_2','weight']
-# name_to_node = {name: i for i, name in enumerate(np.unique(df[["node_1", "node_2"]].values))}
-# n_nodes = len(name_to_node)
-# A = np.zeros((n_nodes, n_nodes))
-# for row in df.itertuples():
-#     n1 = name_to_node[row.node_1]
-#     n2 = name_to_node[row.node_2]
-#     A[n1, n2] += row.weight
-#     A[n2, n1] += row.weight
-# print(A)
