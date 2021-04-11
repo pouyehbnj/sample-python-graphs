f02ec6410309a08fa831f341cb52d8280e585d20
pouyehbnj <pouyehbnj@gmail.com>
2021-04-11 12:38:45 -0700

    selecting required colomns of csv file added

--- graphs.py
+++ graphs.py
@@ -1,13 +1,3 @@
 import pandas as pd
-df = pd.read_csv('./soc-sign-bitcoinalpha.csv', names=['source','destination','weight'])
+df = pd.read_csv('./soc-sign-bitcoinalpha.csv',usecols=[0,1,2])
 print(df)
-#df.columns = ['node_1','node_2','weight']
-# name_to_node = {name: i for i, name in enumerate(np.unique(df[["node_1", "node_2"]].values))}
-# n_nodes = len(name_to_node)
-# A = np.zeros((n_nodes, n_nodes))
-# for row in df.itertuples():
-#     n1 = name_to_node[row.node_1]
-#     n2 = name_to_node[row.node_2]
-#     A[n1, n2] += row.weight
-#     A[n2, n1] += row.weight
-# print(A)
