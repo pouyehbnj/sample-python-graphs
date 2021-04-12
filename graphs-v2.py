7326b12197b4510e67eb4820793658f4587c4d69
zahradehghanpur <zahradehghanpour>
2021-04-12 14:25:06 +0430

    add dijkstra test

--- graphs-v2.py
+++ graphs-v2.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 from bokeh.plotting import figure, output_file, show
 from random import sample
+import sys
 inf = float("inf")
 #loading data to dataframe
 df = pd.read_csv('./soc-sign-bitcoinalpha.csv' ,  names=['source','destination','weight'] ,usecols=[0,1,2])
@@ -76,14 +77,71 @@ p = figure(plot_width=400, plot_height=400 )
 p.line(avgDeg, nodes_y, line_width=2)
 show(p)
 
+
+
+
+
+## test shortest path
+def printSolution(dist):
+    print("Vertex tDistance from Source")
+    for node in range(V):
+        print(node, "t", dist[node])
+
+# A utility function to find the vertex with
+# minimum distance value, from the set of vertices
+# not yet included in shortest path tree
+def minDistance(dist, sptSet , V):
+    V = len(nodes_y)
+    # Initilaize minimum distance for next node
+    min = sys.maxsize
+
+    # Search not nearest vertex not in the
+    # shortest path tree
+    for v in range(V):
+        if dist[v] < min and sptSet[v] == False:
+            min = dist[v]
+            min_index = v
+
+    return min_index
+
+# Funtion that implements Dijkstra's single source
+# shortest path algorithm for a graph represented
+# using adjacency matrix representation
+def dijkstra(src , graph , V):
+    
+    dist = [sys.maxsize] * V
+    dist[src] = 0
+    sptSet = [False] * V
+
+    for cout in range(V):
+
+        # Pick the minimum distance vertex from
+        # the set of vertices not yet processed.
+        # u is always equal to src in first iteration
+        u = minDistance(dist, sptSet ,V)
+
+        # Put the minimum distance vertex in the
+        # shotest path tree
+        sptSet[u] = True
+
+        # Update dist value of the adjacent vertices
+        # of the picked vertex only if the current
+        # distance is greater than new distance and
+        # the vertex in not in the shotest path tree
+        for v in range(V):
+            if graph[u][v] > 0 and sptSet[v] == False and dist[v] > dist[u] + graph[u][v]:
+                dist[v] = dist[u] + graph[u][v]
+
+    printSolution(dist)
+
 # Shortest path algorithm-Dijkstra 
     #def Dijkstra(self, v0):
     # # Initialization operation
-
-# vertexn = len(nodes_y)
+dijkstra(1,nodes,len(nodes_y))
+#vertexn = len(nodes_y)
 # vertexes = nodes_y
 # arcs = nodes
-# v0=1581
+# v0=1
 # D = [inf]*vertexn  # Used to store the shortest path length from vertex v0 to v
 # path = [None]*vertexn  # Used to store the path from vertex v0 to v
 # final = [None]*vertexn  # Indicates whether the shortest path from v0 to v has been found
@@ -109,4 +167,6 @@ show(p)
 #             D[k] = min+arcs[v][k]
 #             path[k] = str(path[v])+","+str(vertexes[k])
 # #return D, path
-# print(D)
+# #print(D)
+# #print(nodes)
+# #print(nodes_y)
