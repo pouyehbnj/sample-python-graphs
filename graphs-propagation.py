fab9f3602e23e1af982646f9cf74edf324ed9ae2
pouyehbnj <pouyehbnj@gmail.com>
2021-04-17 15:23:58 +0430

    code for coloring through with most degreed added

--- graphs-propagation.py
+++ graphs-propagation.py
@@ -53,7 +53,21 @@ def color_graph(node, s, iteration):
 s = set()
 random_node = random.sample(set(list(Graph.nodes())), 1)[0]
 color_graph(random_node, s, 0)
-output_file("CommonNeighbours.html")
+output_file("randomNode.html")
+
+p = figure(plot_width=400, plot_height=400)
+p.line(iteration_x,percentage_y, line_width=2)
+
+show(p)
+
+s.clear()
+iteration_x.clear()
+percentage_y.clear()
+most_degreed_node = list(sorted(Graph.degree, key=lambda x: x[1], reverse=True))[0][0]
+
+print("the most degreed node",str(most_degreed_node))
+color_graph(most_degreed_node,s,0)
+output_file("mostDegreed.html")
 
 p = figure(plot_width=400, plot_height=400)
 p.line(iteration_x,percentage_y, line_width=2)
